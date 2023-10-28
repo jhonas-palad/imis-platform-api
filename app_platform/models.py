c552567e6361be0dc915f22085e7f48054002093
Jhonas Palad <jhonasemmanuel@gmail.com>
2023-10-28 21:20:23 +0800

    model user migrated

--- app_platform/models.py
+++ app_platform/models.py
@@ -23,3 +23,47 @@ class Address(models.Model):
     state = models.CharField(max_length=100, blank=False, null=False)
     postal_code = models.CharField(max_length=20, blank=False, null=False)
     
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    #Use the indexes option instead.
+    #Where possible, use the Meta.indexes option instead. In nearly all cases, indexes provides more functionality than db_index. db_index may be deprecated in the future.
+    email = models.EmailField(_("email address"), unique=True, db_index=True) 
+    phone_number = models.CharField(_("phone number"), max_length=20, null=False, blank=False)
+    # profile_img = models.FileField(
+    #     max_length=1000, upload_to=img_url,
+    #     null = True, blank=True
+    # )
+    
+    #Same as django auth model AbstractUser
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    
+    gender = models.CharField(
+        choices=GENDER_TYPES,
+        max_length=10,
+        blank=True,
+        null=True
+    )
+    address = models.ManyToManyField(Address)
+    dob = models.DateField(blank=True, null=True)
+    
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    email_verified = models.BooleanField(_("email verified"),default=False)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["phone_number"]
+
+    objects = UserManager()
--- imis/settings.py
+++ imis/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 #AUTHENTICATION
 
-# AUTH_USER_MODEL = "app_platform.User"
+AUTH_USER_MODEL = "app_platform.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
